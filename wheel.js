449749db915106168a8d51d9a7dc6347ee446b31
Matthew Niemer <matthewniemer2018@u.northwestern.edu>
2018-01-03 23:06:59 -0600

    created second tab

--- wheel.js
+++ wheel.js
@@ -1,12 +1,27 @@
-var img = document.getElementById('wheel')
-img.addEventListener('click', onClick, false);
+var img = document.getElementById('wheel');
+var count = 0;
+img.addEventListener('click', spinWheel, false);
 
-function onClick() {
+function spinWheel() {
+    count = count + 2;
     this.removeAttribute('style');
-    
-    var deg = 500 + Math.round(Math.random() * 500);
-    var css = '-webkit-transform: rotate(' + deg + 'deg);';
+    var deg = (count*365) + Math.round(Math.random() * 365);
+    var css = 'transform: rotate(' + deg + 'deg);';
     this.setAttribute(
         'style', css
     );
+}
+
+function openTab(evt, tabName) {
+    var i, tabContent, tabButton;
+    tabContent = document.getElementsByClassName("tabContent");
+    for (i = 0; i < tabContent.length; i++) {
+        tabContent[i].style.display = "none";
+    }
+    tabButton = document.getElementsByClassName("tabButton");
+    for (i = 0; i < tabButton.length; i++) {
+        tabButton[i].className = tabButton[i].className.replace(" active", "");
+    }
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
 }
